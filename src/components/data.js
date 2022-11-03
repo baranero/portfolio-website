6a28b227ed3610ac2fc259a1d5111939d801a20f
baranero <jakub.baran2@gmail.com>
2022-11-03 15:49:03 +0100

    Deploy

---
+++ src/components/data.js
@@ -0,0 +1,37 @@
+export const projects = [
+  {
+    title: "Eat IT",
+    subtitle: "React Hooks",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./culinary-recipes.png",
+    link: "https://baranero.github.io/culinary-recipes/",
+  },
+  {
+    title: "Sign up form",
+    subtitle: "React Hooks",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./signup-form.png",
+    link: "https://baranero.github.io/signup-form/",
+  },
+  {
+    title: "My travel journal",
+    subtitle: "React Hooks",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./travel-journal.png",
+    link: "https://baranero.github.io/travel-journal/",
+  },
+  {
+    title: "The Hangman",
+    subtitle: "Vanilla JavaScript",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./hangman.png",
+    link: "https://baranero.github.io/hangman/",
+  },
+];
+
+
+export const skills = ['HTML5', 'CSS3', 'JavaScript', 'React', 'GIT', 'GitHub', 'REST API', 'Tailwind CSS']
