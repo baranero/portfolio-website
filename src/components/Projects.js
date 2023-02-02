d6ace3a35847b83d4b32836fb1efb9e2a3bc7d80
baranero <jakub.baran2@gmail.com>
2023-02-02 23:30:12 +0100

    projects text fix

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -22,14 +22,14 @@ export default function Projects() {
               key={project.image}
               rel="noreferrer"
               target="_blank"
-              className="sm:w-1/2 w-100 p-4">
+              className="sm:w-1/2 w-full p-4">
               <div className="flex relative">
                 <img
                   alt="gallery"
                   className="absolute py-2 inset-0 w-full h-60 object-cover object-center"
                   src={project.image}
                 />
-                <div className="px-8 py-10 relative z-10 w-full h-60 border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
+                <div className="px-8 py-2 relative z-10 w-full h-60 border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
                   <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
                     {project.subtitle}
                   </h2>
