cee2900c06fb6ab0750ac88f09b744226b2dffb1
baranero <jakub.baran2@gmail.com>
2023-05-30 23:55:24 +0200

    project changes

---
+++ src/data.js
@@ -0,0 +1,41 @@
+export const projects = [
+  {
+    title: "FireApp",
+    subtitle: "Next.js, Typescript, PostgreSQL, Prisma",
+    description:
+      "App for managing the team in the Fire Department",
+    image: "./users-info.png",
+    link: "https://fd-app-eight.vercel.app/",
+    github: "https://github.com/baranero/fd-app"
+  },
+  {
+    title: "Colors Filter",
+    subtitle: "Typescript, SCSS",
+    description:
+      "Pick colors by RGB HEX and filter them.",
+    image: "./users-info.png",
+    link: "https://baranero.github.io/colors-filter/",
+    github: "https://github.com/baranero/colors-filter"
+  },
+  {
+    title: "Space Web",
+    subtitle: "React Hooks, React Router, Sass",
+    description:
+      "Website design made on the basis of a graphic design. Fully responsive.",
+    image: "./space-web.png",
+    link: "https://baranero.github.io/space-web-react-scss/",
+    github: "https://github.com/baranero/space-web-react-scss"
+  },
+  {
+    title: "Eat IT",
+    subtitle: "React with API",
+    description:
+      "An application that allows you to generate recipes based on the entered data.",
+    image: "./culinary-recipes.png",
+    link: "https://baranero.github.io/culinary-recipes/",
+    github: "https://github.com/baranero/culinary-recipes"
+  },
+];
+
+
+export const skills = ['HTML5', 'CSS3', 'Saas', 'JavaScript', 'React', 'Redux', 'TypeScript', 'Next.js', 'GIT', 'GitHub', 'Databases']
